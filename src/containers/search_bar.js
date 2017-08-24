696e483838db463d0e5c36b7b77becc00ca774ab
papistan <jpapisan@gmail.com>
2017-08-24 12:25:28 -0700

    create weather api call with axioswithin action

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -10,13 +10,21 @@ export default class SearchBar extends Component {
 	}
 
 	onInputChange(event) {
-		console.log(event.target.value);
+		
 		this.setState({term: event.target.value})
 	}
 
+	onFormSubmit(event) {
+		event.preventDefault();
+
+		// get weather data
+		// api.openweathermap.org/data/2.5/forecast?id=524901&APPID=
+	}
+
+
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
 					placeholder="Get a five-day forecast in your favorite cities."
 					className="form-control"
